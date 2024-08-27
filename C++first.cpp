502e236942fd71a3c5126f34bc21d92d4fd8034b
jacov677 <60140972+jacov677@users.noreply.github.com>
2024-08-27 18:12:45 +0200

    Add files via upload

---
+++ C++first.cpp
@@ -0,0 +1,20 @@
+//Description signs # in python
+//this is a demonstraton of c++ structure
+#include<iostream>;
+//start menu
+using namespace std //#standard library identities
+
+
+
+int main()        //this is a function
+{
+    //execution code goes here
+    count<<"hello World";
+    system("pause70";)
+
+
+
+
+
+
+}
